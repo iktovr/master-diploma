11febdbb5e154cb124126551420cc500605a719a
iktovr <vikvladbir@mail.ru>
2026-04-05 23:59:33 +0300

    now c++ is my best friend

---
+++ src/agent.h
@@ -0,0 +1,7 @@
+struct Agent {
+    int id;
+    double x;
+    double y;
+    int edge_id;
+    double t;
+};
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "agent.h"
+
+#include <iostream>
+
+int main() {
+    return 0;
+}
